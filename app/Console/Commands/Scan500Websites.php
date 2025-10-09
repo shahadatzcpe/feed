4dae92b3cf06f0ef47369b9a9fd3618b18d43da5
Md Shahadat Hossain <shahadat.zcpe@gmail.com>
2025-10-10 00:22:36 +0100

    Laravel & Horizon Installed

---
+++ app/Console/Commands/Scan500Websites.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class Scan500Websites extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:scan500-websites';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+
+    }
+}
