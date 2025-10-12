62eff1b39b101d519d5cc0c6648cc6df55438599
Md Shahadat Hossain <shahadat.zcpe@gmail.com>
2025-10-12 14:57:09 +0100

    Push code

---
+++ app/Jobs/TestFrequentJob.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Support\Facades\Log;
+
+class TestFrequentJob implements ShouldQueue
+{
+    use Dispatchable, Queueable;
+
+    protected $job_id;
+
+    public function __construct($job_id) {
+        $this->job_id = $job_id;
+    }
+    public function handle()
+    {
+        context([
+            'queue' => 'frequent_256mb_1hr',
+            'job_id' => $this->job_id
+        ]);
+
+        Log::info("Running TestFrequentJob on process " . getmypid());
+        sleep(5); // Simulate some work
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Jobs\TestFrequentJob;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('/test-frequent', function () {
+    for ($i = 0; $i < (request()->n ?: 10); $i++) {
+        TestFrequentJob::dispatch(now() -> toString() . "--" . $i);//->onQueue(request()->queue ?: 'frequent_256mb_1hr');
+    }
+    return 'Dispatched 100 frequent jobs!';
 });
