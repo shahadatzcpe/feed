1894756717f960f03ec992f842b615e493e1527d
Md Shahadat Hossain <shahadat.zcpe@gmail.com>
2025-10-23 15:42:24 +0100

    imagedefinitions updated

--- config/database.php
+++ config/database.php
@@ -163,6 +163,7 @@ return [
             'backoff_algorithm' => env('REDIS_BACKOFF_ALGORITHM', 'decorrelated_jitter'),
             'backoff_base' => env('REDIS_BACKOFF_BASE', 100),
             'backoff_cap' => env('REDIS_BACKOFF_CAP', 1000),
+            'scheme' => env('REDIS_SCHEME', 'tcp'),
         ],
 
         'cache' => [
