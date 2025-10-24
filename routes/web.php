4531e77e93a2681c5c542b5b845c3361980a8b72
Md Shahadat Hossain <shahadat.zcpe@gmail.com>
2025-10-24 23:31:24 +0100

    image definitions updated

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,14 @@ Route::get('/', function () {
 
 Route::get('/test-frequent', function () {
     for ($i = 0; $i < (request()->n ?: 10); $i++) {
-        TestFrequentJob::dispatch(now() -> toString() . "--" . $i)->onQueue(request()->queue ?: 'default');
+        TestFrequentJob::dispatch(now() -> toString() . "--" . $i)->onQueue(request()->queue ?: [
+            'default',
+            'default_priority',
+            'heavy_priority_1gb_3hr',
+            'heavy_1gb_3hr',
+            'long_priority_512mb_12hr',
+            'long_512mb_1hr'
+        ][rand(0,5)]);
     }
     return 'Dispatched 100 frequent jobs!';
 });
